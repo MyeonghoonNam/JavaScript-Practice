f7d448127d66c975c82130ce22132c70519f8a71
MyeongHoonNam <codenmh0822@naver.com>
2021-08-10 15:11:05 +0900

    reduce 사용자 정의 구현

--- reduce.js
+++ reduce.js
@@ -9,3 +9,25 @@ const numbersSum = numbers.reduce((accumulator, currentValue, currentIndex, arra
 });
 
 console.log(numbersSum);
+
+// reduce 사용자 정의
+const reduce = (f, acc, iter) => {
+  if(!iter) {
+    iter = acc[Symbol.iterator]();
+    acc = iter.next().value;
+  }
+
+  for(const i of iter) {
+    acc = f(acc, i);
+  }
+
+  return acc;
+}
+
+const add = (a, b) => a + b;
+
+// 초기값 지정한 경우의 reduce
+console.log(reduce(add, 0, [1, 2, 3, 4, 5]));
+
+// 초기값 지정하지 않은 경우의 reduce
+console.log(reduce(add, [1, 2, 3, 4, 5]));
