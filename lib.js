5094b790a95553d9c808bb2a8935bbfbc2ca9145
MyeongHoonNam <codenmh0822@naver.com>
2021-08-10 16:54:39 +0900

    pipe 함수 구현

--- lib.js
+++ lib.js
@@ -29,3 +29,24 @@ const reduce = (f, acc, iter) => {
 
   return acc;
 }
+
+const add = (a, b) => a + b;
+
+const go = (...args) => reduce((a, f) => f(a), args); 
+
+go(
+  add(0, 1),
+  a => a + 10,
+  a => a + 100,
+  console.log
+);
+
+const pipe = (f, ...fs) => (...as) => go(f(...as), ...fs);
+
+const f = pipe(
+  (a, b) => a + b,
+  a => a + 10,
+  a => a + 100
+)
+
+console.log(f(0, 1));
