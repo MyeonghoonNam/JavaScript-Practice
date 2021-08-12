d9b125d676317b850afb782e7c3544eff660909d
MyeongHoonNam <codenmh0822@naver.com>
2021-08-12 20:07:12 +0900

    즉시, 지연, Promise, 병렬성 조합하기

--- functionalProgramming/async.js
+++ functionalProgramming/async.js
@@ -400,10 +400,30 @@ const flatMap = curry(pipe(L.flatMap, take(Infinity)));
 // --------------------------------
 
 // 즉시 병렬적으로 평가하기 - C.map, C.filter
-const delay1000 = a => new Promise(resolve => {
-  console.log('hi');
-  setTimeout(() => resolve(a), 1000)
+// const delay1000 = a => new Promise(resolve => {
+//   console.log('hi');
+//   setTimeout(() => resolve(a), 1000)
+// });
+
+// C.map(a => delay1000(a * a), [1, 2, 3, 4]).then(console.log); // [1, 4, 9, 16]
+// C.filter(a => delay1000(a % 2), [1, 2, 3, 4]).then(console.log); // [1, 3]
+
+// --------------------------------
+
+
+// 즉시, 지연, Promise, 병렬성 조합하기
+
+const delay500 = (a, name) => new Promise(resolve => {
+  console.log(`${name} : ${a}`);
+  setTimeout(() => resolve(a), 500)
 });
 
-C.map(a => delay1000(a * a), [1, 2, 3, 4]).then(console.log); // [1, 4, 9, 16]
-C.filter(a => delay1000(a % 2), [1, 2, 3, 4]).then(console.log); // [1, 3]
+console.time('');
+go([1, 2, 3, 4, 5, 6, 7, 8],
+  L.map(a => delay500(a * a, 'map 1')),
+  L.filter(a => delay500(a % 2, 'filter 2')),
+  L.map(a => delay500(a + 1, 'map 3')),
+  C.take(4),
+  reduce(add),
+  console.log,
+  _ => console.timeEnd(''))
