10ed0734544da313a1b09a32800b621dc290c06e
MyeongHoonNam <codenmh0822@naver.com>
2021-08-18 18:59:43 +0900

    feat : SimpleToDoList - toDoList

---
+++ component/src/toDoList.js
@@ -0,0 +1,21 @@
+function toDoList({ target, initialState }) {
+  const toDoList = document.createElement('div');
+
+  target.appendChild(toDoList);
+
+  this.state = initialState;
+
+  this.setState = (nextState) => {
+    (this.state = nextState), this.render();
+  };
+
+  this.render = () => {
+    toDoList.innerHTML = `
+      <ul>
+        ${this.state.map(({ text }) => `<li>${text}</li>`).join('')}
+      </ul>
+    `;
+  };
+
+  this.render();
+}
