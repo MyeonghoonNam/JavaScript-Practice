fae390fadb05969e56d8cf1c50a96f8613c91fd5
MyeongHoonNam <codenmh0822@naver.com>
2021-08-18 18:58:28 +0900

    쿠키 사용 이해하기

--- browser/storage/index.js
+++ browser/storage/index.js
@@ -1,6 +1,9 @@
 // 쿠키 관리는 String으로 하며 불편하다.
-document.cookie = 'key1=value1;';
+document.cookie = 'key1=value1';
+document.cookie = 'key2=value2';
+
 console.log(document.cookie);
+console.log(document.cookie.split(';')); // 전체 쿠키정보의 분리가 필요하다.
 
 // 로컬 스토리지
 // 데이터를 저장한다.
