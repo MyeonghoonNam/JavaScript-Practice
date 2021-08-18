e720f2e86655337283b0b60870e0fef955cc6b6d
MyeongHoonNam <codenmh0822@naver.com>
2021-08-18 18:59:32 +0900

    feat : SimpleToDoList - toDoForm

---
+++ component/src/toDoForm.js
@@ -0,0 +1,30 @@
+function toDoForm({ target, onSubmit }) {
+  const form = document.createElement('form');
+
+  target.appendChild(form);
+
+  let isInit = false;
+
+  this.render = () => {
+    form.innerHTML = `
+      <input type="text" name="todo">
+      <button>Add</button>
+    `;
+
+    if (!isInit) {
+      form.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        const input = form.querySelector('input[name=todo]');
+        const text = input.value;
+
+        if (text.length > 1) {
+          input.value = '';
+          onSubmit(text);
+        }
+      });
+    }
+  };
+
+  this.render();
+}
