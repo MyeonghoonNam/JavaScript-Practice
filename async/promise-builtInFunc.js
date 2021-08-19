ac14afedc92eb5e339cbc6c45db3e4b66c63fd11
MyeongHoonNam <codenmh0822@naver.com>
2021-08-19 16:21:32 +0900

    promise 내장함수 : allSettled

--- async/promise-builtInFunc.js
+++ async/promise-builtInFunc.js
@@ -57,3 +57,27 @@
 // Promise.any(promise).then((message) => console.log(message));
 
 // --------------------------------------------------------
+
+// Promise.allSettled
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+const promise = [1, 2, 3, 4, 5].map((n) => {
+  const delayTime = getRandomInt(1000, 5000);
+
+  return new Promise((resolve, reject) => {
+    if (n % 2 === 0) {
+      return reject(`${n}번 고양이 완주 실패 ㅠ ㅠ!`);
+    }
+
+    setTimeout(() => {
+      resolve(`${n}번 고양이 승리 !`);
+    }, delayTime);
+  });
+});
+
+Promise.allSettled(promise).then((result) => console.log(result));
