3e4d007fdeee83044cd314aeb646ccde5d1346b1
MyeongHoonNam <codenmh0822@naver.com>
2021-08-19 15:38:28 +0900

    Promise 내장함수 : all

---
+++ async/promise-builtInFunc.js
@@ -0,0 +1,10 @@
+// Promise.all
+// const delay = (time) => new Promise((resolve) => setTimeout(resolve, time));
+
+// const promise1 = delay(3000);
+// const promise2 = delay(4000);
+// const promise3 = delay(5000);
+
+// Promise.all([promise1, promise2, promise3]).then(() => {
+//   console.log('work Done !');
+// });
