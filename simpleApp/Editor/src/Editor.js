2344ddf28a23c2466d157189eba0058ee1b4d1f5
MyeongHoonNam <codenmh0822@naver.com>
2021-08-26 03:23:51 +0900

    feat: Editor Component 구현

---
+++ simpleApp/Editor/src/Editor.js
@@ -0,0 +1,46 @@
+export default function Editor({target, initialState = {
+  title: '',
+  content: ''
+}, onEditing}) {
+  const editor = document.createElement('div');
+  let isInitialize = false
+
+  target.appendChild(editor);
+
+  this.state = initialState;
+
+  this.setState = (nextState) => {
+    this.state = nextState;
+    editor.querySelector('[name=title]').value = this.state.title;
+    editor.querySelector('[name=content]').value = this.state.content;
+    this.render(); 
+  }
+
+  this.render = () => {
+    if(!isInitialize) {
+      editor.innerHTML = `
+        <input type="text" name="title" style="width:600px" value="${this.state.title}" />
+        <textarea name="content" style="width:600px; height:400px">${this.state.content}</textarea>
+      `
+
+      isInitialize = true;
+    }
+  }
+
+  this.render();
+
+  editor.addEventListener('keyup', e => {
+    const {target} = e;
+    const name = target.getAttribute('name');
+
+    if(this.state[name] !== undefined) {
+      const nextState = {
+        ...this.state,
+        [name]: target.value
+      }
+
+      this.setState(nextState);
+      onEditing(this.state);
+    }
+  })
+}
