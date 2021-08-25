2667cc5f409d30048539afb35ee071b7e8f3f576
MyeongHoonNam <codenmh0822@naver.com>
2021-08-26 03:24:26 +0900

    feat: PostsPage Component 구현

---
+++ simpleApp/Editor/src/PostsPage.js
@@ -0,0 +1,26 @@
+import {request} from "./api.js";
+import PostList from "./PostList.js";
+
+export default function PostPage({target}) {
+  const page = document.createElement('div');
+  const postList = new PostList({
+    target,
+    initialState: []
+  });
+
+  const newPostButton = document.createElement('button')
+  newPostButton.textContent = 'New Post';
+
+  page.appendChild(newPostButton);
+
+  const fetchPosts = async () => {
+    const posts = await request('/posts');
+
+    postList.setState(posts);
+  }
+
+  this.render = async () => {
+    await fetchPosts();
+    target.appendChild(page);
+  }
+}
