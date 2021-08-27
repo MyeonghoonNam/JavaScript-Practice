358280968c3c73aa6a598ba6a55b558514fba1d7
MyeongHoonNam <codenmh0822@naver.com>
2021-08-27 16:42:35 +0900

    style: footer 전체 스타일 적용

--- component/src/toDoForm.js
+++ component/src/toDoForm.js
@@ -4,23 +4,28 @@ export default function toDoForm({ target, onSubmit }) {
       throw new Error('경고 : toDoForm 컴포넌트를 new로 생성해주세요 !')
     }
   
+    const footer = document.createElement('footer');
     const form = document.createElement('form');
-  
-    target.appendChild(form);
+
+    target.appendChild(footer);
+    footer.appendChild(form);
   
     let isInit = false;
   
     this.render = () => {
       form.innerHTML = `
-        <input type="text" name="todo">
-        <button>Add</button>
+        <input class="footer__input" type="text" name="todoText" placeholder="Add to List..."
+        autocomplete="off"/>
+        <button class="footer__button">
+          <i class="fas fa-plus" aria-hidden="true"></i>
+        </button>
       `;
   
       if (!isInit) {
         form.addEventListener('submit', (e) => {
           e.preventDefault();
   
-          const input = form.querySelector('input[name=todo]');
+          const input = form.querySelector('input[name=todoText]');
           const text = input.value;
   
           if (text.length > 1) {
