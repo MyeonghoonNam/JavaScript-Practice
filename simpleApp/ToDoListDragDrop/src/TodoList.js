3219da5a0ca93e5b9cb181e36056957b2e014e04
MyeongHoonNam <codenmh0822@naver.com>
2021-09-13 18:08:00 +0900

    feat: api 호출 예외처리 구현

--- simpleApp/ToDoListDragDrop/src/TodoList.js
+++ simpleApp/ToDoListDragDrop/src/TodoList.js
@@ -1,5 +1,6 @@
-export default function TodoList({ target, initialState }) {
+export default function TodoList({ target, initialState, onDrop }) {
   const todoList = document.createElement('div');
+  todoList.setAttribute('droppable', 'true');
 
   target.appendChild(todoList);
 
@@ -16,11 +17,40 @@ export default function TodoList({ target, initialState }) {
     todoList.innerHTML = `
       <h2>${title}</h2>
       <ul>
-        ${todos.map((todo) => `<li>${todo.content}</li>`).join('')}
+        ${todos
+          .map(
+            (todo) =>
+              `<li data-id="${todo._id}" draggable="true">${todo.content}</li>`
+          )
+          .join('')}
       </ul>
       ${todos.length === 0 ? '설정된 일이 없습니다.' : ''}
     `;
   };
 
   this.render();
+
+  todoList.addEventListener('dragstart', (e) => {
+    const li = e.target.closest('li');
+
+    e.dataTransfer.setData('todoId', li.dataset.id);
+  });
+
+  todoList.addEventListener('dragover', (e) => {
+    e.preventDefault();
+    e.dataTransfer.dropEffect = 'move';
+  });
+
+  todoList.addEventListener('drop', (e) => {
+    e.preventDefault();
+
+    const droppedTodoId = e.dataTransfer.getData(todoId);
+
+    // 현재 TodoList의 Todo가 아닌 경우 상위 컴포넌트에 알림
+    const { todos } = this.state;
+
+    if (!todos.find((todo) => todo.id === droppedTodoId)) {
+      onDrop(droppedTodoId);
+    }
+  });
 }
