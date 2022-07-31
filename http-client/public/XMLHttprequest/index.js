d0a74d9dfcbc779d2fbc48e8b22e2cbe7061f484
MyeongHoonNam <codenmh0822@naver.com>
2022-07-31 21:58:17 +0900

    feat: 이벤트 바인딩 구현

---
+++ http-client/public/XMLHttprequest/index.js
@@ -0,0 +1,52 @@
+import { getTodos, createTodo, updateTodo, deleteTodo } from "./fetchTodos.js";
+
+const TODO_TEXT = "Simple Todo";
+
+const printResult = (action, result) => {
+  const time = new Date().toLocaleTimeString();
+  const node = document.createElement("p");
+
+  node.textContent = `${action}: ${JSON.stringify(result)} (${time})`;
+  document.querySelector("div").appendChild(node);
+};
+
+const onListClick = async () => {
+  const result = await getTodos();
+  printResult("List Todos", result);
+};
+
+const onAddClick = async () => {
+  const result = await createTodo(TODO_TEXT);
+  printResult("Create Todo", result);
+};
+
+const onUpdateClick = async () => {
+  const todos = await getTodos();
+  const { id, completed } = todos[0];
+  const newTodo = {
+    id,
+    completed: !completed,
+  };
+  const result = await updateTodo(newTodo);
+  printResult("Update First Todo", result);
+};
+
+const onDeleteClick = async () => {
+  const todos = await getTodos();
+  const { id } = todos[0];
+  await deleteTodo(id);
+  printResult("Delete First Todo");
+};
+
+document
+  .querySelector("button[data-list]")
+  .addEventListener("click", onListClick);
+document
+  .querySelector("button[data-add]")
+  .addEventListener("click", onAddClick);
+document
+  .querySelector("button[data-update]")
+  .addEventListener("click", onUpdateClick);
+document
+  .querySelector("button[data-delete]")
+  .addEventListener("click", onDeleteClick);
