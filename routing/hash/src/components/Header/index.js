6e7b379003a91339eff680ce118f75f71eeafc58
MyeongHoonNam <codenmh0822@naver.com>
2022-08-03 15:48:17 +0900

    feat: Header 버튼을 통한 프로그래밍 방식 라우팅 기능 구현

--- routing/hash/src/components/Header/index.js
+++ routing/hash/src/components/Header/index.js
@@ -1,14 +1,37 @@
+const NAV_BTN_SELECTOR = "button[data-navigate]";
 class Header {
-  render() {
-    const target = document.createElement("header");
-    const template = `
-      <a href="#/">Go To Index</a>
-      <a href="#/list">Go To List</a>
-      <a href="#/dummy">Dummy Page</a>
-    `;
-    target.innerHTML = template;
+  constructor() {
+    this.header = document.createElement("header");
+    this.bindEvents();
+  }
 
-    return target;
+  bindEvents() {
+    this.header.addEventListener("click", (e) => {
+      const { target } = e;
+
+      if (target.matches(NAV_BTN_SELECTOR)) {
+        const { navigate } = target.dataset;
+        window.location.hash = navigate;
+      }
+    });
+  }
+
+  render() {
+    const template = `
+      <button data-navigate="/">
+          Go To Home
+      </button>
+      <button data-navigate="/list">
+          Go To List
+      </button>
+      <button data-navigate="/NotFound">
+        NotFound Page
+      </button>
+    `;
+
+    this.header.innerHTML = template;
+
+    return this.header;
   }
 }
 
