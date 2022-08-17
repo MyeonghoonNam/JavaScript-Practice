1efb0d48ee4ae3093cdfbe461f4277531de5ad86
MyeongHoonNam <codenmh0822@naver.com>
2022-08-17 21:59:51 +0900

    reducer 구현

---
+++ stateManagement/src/store/reducer.js
@@ -0,0 +1,24 @@
+import { TODO_INSERT } from "./action.js";
+
+const INITIAL_STATE = {
+  todos: [],
+  currentFilter: "All",
+};
+
+const reducer = (state = INITIAL_STATE, action) => {
+  if (!action) {
+    return { ...state };
+  }
+
+  const { type, payload } = action;
+
+  switch (type) {
+    case TODO_INSERT:
+      return {
+        ...state,
+        todos: state.todos.concat({ payload }),
+      };
+  }
+};
+
+export { reducer };
